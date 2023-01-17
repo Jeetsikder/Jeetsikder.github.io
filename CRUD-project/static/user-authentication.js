e330953ed5fadf4b7c522e341fe01cada817a8e4
Jeet Sikder <jeetsikder78@gmail.com>
2023-01-17 14:29:59 +0530

    js add

---
+++ CRUD-project/static/user-authentication.js
@@ -0,0 +1,23 @@
+document.getElementById("emailId").value = "";
+document.getElementById("password").value = "";
+let emailId = document.getElementById("emailId");
+let password = document.getElementById("password");
+let submit = document.getElementById("submit");
+let link = `http://localhost:8000/form/list-type/verify`;
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  let data;
+  data = { emailId: emailId.value, password: password.value };
+  console.log(JSON.stringify(data));
+  fetch(link, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-type": "application/json" },
+  })
+    .then((response) => response.text())
+    .then((response) => {
+      console.log(response);
+      loation.replace(`${response}`);
+    })
+    .catch((errorFetch) => console.log(errorFetch));
+});
